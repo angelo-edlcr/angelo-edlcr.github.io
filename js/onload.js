fdb19aec3574b61e2403775db797afdb74ca8a81
Angelo De La Cruz <99463079+angelo-edlcr@users.noreply.github.com>
2022-08-08 02:25:36 -0300

    Fix responsive design

--- js/onload.js
+++ js/onload.js
@@ -2,6 +2,6 @@ window.addEventListener("load", function() {
     document.getElementById("loading").style.display = "none";
     setTimeout(() => {
         this.document.getElementById("myImg").style.animation = "myImgCircle 1s";
-    }, 1000);
+    }, 2000);
     
 });
