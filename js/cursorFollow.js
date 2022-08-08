758448cb91939d76d4b1d7c1c6346bde6ad1c7ae
Angelo De La Cruz <99463079+angelo-edlcr@users.noreply.github.com>
2022-08-08 02:29:52 -0300

    Cursor style deleted

--- js/cursorFollow.js
+++
@@ -1,7 +0,0 @@
-const mouseCursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove", (e)=>{
-    mouseCursor.style.top = e.clientY + 'px';
-    mouseCursor.style.left = e.clientX + 'px';
-});
-
